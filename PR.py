6dff60e614799b4debb29fe59ee63d82bff366fa
Edselph <edsel.harren@binus.ac.id>
2020-09-21 20:30:54 +0700

    PR_Excersice_Week_2

---
+++ PR.py
@@ -0,0 +1,58 @@
+# Pr no 1
+
+# degree = int(input("Put Number:"))
+# angle = degree
+# radians = (degree) * 3.14 / 180
+# print("degrees:"+  str (degree))
+# print("radians:"+ str(radians))
+
+# Pr no 2
+
+# student1 = int(input("score no 1: "))
+# student2 = int(input("score no 2: "))
+# student3 = int(input("score no 3: "))
+# average = int((student1 + student2 + student3 )/3)
+# print("Student scores:")
+# print("Student 1 :" + str(student1))
+# print("Student 2 :" + str(student2))
+# print("Student 3 :" + str(student3))
+# print("average:" + str(average))
+
+# Pr no 3
+
+# class_a = int(32)
+# class_b = int(45)
+# class_c = int(51)
+# class_a_first = (int(class_a)//5)
+# class_b_first = (int(class_b)//7)
+# class_c_first = (int(class_c)//6)
+# print("Number of students in each group:")
+# print("first class 1 :" + str(class_a_first))
+# print("first class 2 :" + str(class_b_first))
+# print("first class 3 :" + str(class_c_first))
+# print("Number of students leftover:")
+# class_a_second = (int(class_a)%5)
+# class_b_second = (int(class_b)%7)
+# class_c_second = (int(class_c)%6)
+# print("second class 1 :" + str(class_a_second))
+# print("second class 2 :" + str(class_b_second))
+# print("second class 3 :" + str(class_c_second))
+
+# Pr no 4
+
+# pi = 3.14
+# pie_diameter = 55.4
+# pie_radius = pie_diameter / 2
+# circumference = 2 * pi * pie_radius
+# circumference_msg = ( " Jimmy’s pie has a circumference: " )
+# print(circumference_msg, circumference)
+
+# Pr no 5
+
+The_Speed = int(input("the speed number:"))
+The_frequency = int(input("the frequency number:"))
+Lambad = The_Speed/The_frequency
+print("The data : ")
+print("The Speed :",The_Speed,str("m/s"))
+print("The frequency :",The_frequency,str("hz"))
+print("The wavelength :",Lambad,str("M"))
